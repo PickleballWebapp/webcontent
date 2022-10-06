4140f599086ce850051713adf5d01dea07106d16
Joe <joseph.m.napoli@vanderbilt.edu>
2022-10-06 15:50:20 -0500

    Oauth2 Test

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -11,9 +11,24 @@ const awsmobile = {
     "aws_cognito_region": "us-east-1",
     "aws_user_pools_id": "us-east-1_fLn3LpxVA",
     "aws_user_pools_web_client_id": "75tl8hol46rsfg9orjogbtjqpc",
-    "oauth": {},
+    "oauth": {
+        "domain": "ne35io661hc9-dev.auth.us-east-1.amazoncognito.com",
+        "scope": [
+            "phone",
+            "email",
+            "openid",
+            "profile",
+            "aws.cognito.signin.user.admin"
+        ],
+        "redirectSignIn": "https://localhost:3000",
+        "redirectSignOut": "https://localhost:3000",
+        "responseType": "code"
+    },
+    "federationTarget": "COGNITO_USER_POOLS",
     "aws_cognito_username_attributes": [],
-    "aws_cognito_social_providers": [],
+    "aws_cognito_social_providers": [
+        "GOOGLE"
+    ],
     "aws_cognito_signup_attributes": [
         "EMAIL",
         "NAME"
