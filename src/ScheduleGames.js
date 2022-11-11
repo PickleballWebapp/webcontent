ce3d7a7e63d6276b3e3cbe2dc1199bbf34969cba
Jack Harrington <47326372+jharring9@users.noreply.github.com>
2022-11-11 15:02:36 -0600

    Added basic schedule game functionality

--- src/ScheduleGames.js
+++ src/ScheduleGames.js
@@ -9,17 +9,24 @@ import {
   Button,
 } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { UserType } from "./models";
 import { API } from "aws-amplify";
 import { listUsers } from "./graphql/queries";
 import $ from "jquery";
+import { genRoundRobin } from "./Utils";
+import { Modal } from "react-bootstrap";
+
+const userMap = new Map();
 
 export default function ScheduleGames({ user }) {
   const [showAlert, setAlert] = useState(false);
+  const [submissionModal, setModal] = useState(false);
   const [users, setUsers] = useState([]);
   const [teams, setTeams] = useState([]);
+  const [roundRobin, setRoundRobin] = useState([]);
   let navigate = useNavigate();
+  const handleClose = () => setModal(false);
 
   /**
    * Validate that user is a scorer or admin. Fetch user list
@@ -30,9 +37,12 @@ export default function ScheduleGames({ user }) {
       navigate("/scores");
     }
     async function fetchUsers() {
-      setUsers(
-        (await API.graphql({ query: listUsers })).data?.listUsers?.items
-      );
+      await API.graphql({ query: listUsers }).then((response) => {
+        setUsers(response.data.listUsers.items);
+        response.data.listUsers.items.forEach((user) =>
+          userMap.set(user.id, user.name)
+        );
+      });
     }
     fetchUsers();
   }, [navigate, user]);
@@ -78,54 +88,8 @@ export default function ScheduleGames({ user }) {
 
     setAlert(false);
 
-    // todo -- call round-robin algorithm using teams array
-    // todo -- call createGame for each result of the round-robin algo
-  }
-
-  /**
-  * Generate a round robin tournament of form:
-   * tournament[ROUND_#][GAME_#], with each game containing {team1: "TEAM1", team2: "TEAM2"}
-   * So to get the second team of second game of the second round of the tournament:
-   * game2Team2 = tourney[1][1].team2;
-  * */
-  const genRoundRobin = (teams) => {
-    // add "BYE" if needed
-    if (teams.length % 2 === 1) {
-      teams.push(null);
-    }
-
-    const teamCount = teams.length;
-    const rounds = teamCount - 1;
-    const half = teamCount / 2;
-
-    const tournamentPairings = [];
-
-    // get indices so we can iter through our teams
-    const teamIndexes = teams.map((_, i) => i).slice(1);
-
-    for (let round = 0; round < rounds; round++) {
-      const roundPairings = [];
-
-      const newteamIndexes = [0].concat(teamIndexes);
-
-      // get indices for first teams 1 and 2 this round
-      const firstHalf = newteamIndexes.slice(0, half);
-      const secondHalf = newteamIndexes.slice(half, teamCount).reverse();
-
-      // pair our teams up and push as games
-      for (let i = 0; i < firstHalf.length; i++) {
-        roundPairings.push({
-          team1: teams[firstHalf[i]],
-          team2: teams[secondHalf[i]],
-        });
-      }
-
-      // rotating the array to prepare for next round
-      teamIndexes.push(teamIndexes.shift());
-      tournamentPairings.push(roundPairings);
-    }
-
-    return tournamentPairings;
+    setRoundRobin(genRoundRobin(teams));
+    setModal(true);
   }
 
   /**
@@ -156,6 +120,13 @@ export default function ScheduleGames({ user }) {
     const data = [...teams];
     data[index][`user${user1 ? "1" : "2"}`] = value;
     setTeams(data);
+  };
+
+  /**
+   * Create games for round-robin.
+   */
+  const createGames = () => {
+    console.log(roundRobin);
   };
 
   return (
@@ -213,9 +184,13 @@ export default function ScheduleGames({ user }) {
                             handleChange(index, event, false)
                           }
                         >
-                          <option key="empty" value="empty"></option>
+                          <option key="empty" value="empty" name=""></option>
                           {users.map((user) => (
-                            <option key={user.id} value={user.id}>
+                            <option
+                              key={user.id}
+                              value={user.id}
+                              name={user.name}
+                            >
                               {user.name}
                             </option>
                           ))}
@@ -239,12 +214,65 @@ export default function ScheduleGames({ user }) {
           </Row>
           <div className="d-grid gap-2">
             <Button onClick={handleSubmit} className="mb-2">
-              Submit
+              Review and Submit
             </Button>
           </div>
         </Col>
         <Col />
       </Row>
+      <Modal show={submissionModal} onHide={handleClose} size="lg">
+        <Modal.Header closeButton>
+          <Modal.Title>Confirm Round Robin Pairings</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {roundRobin.map((games, round) => (
+            <React.Fragment key={round}>
+              <h5>Round {round + 1}</h5>
+              <Table>
+                <thead>
+                  <tr>
+                    <th>Team 1</th>
+                    <th>Team 2</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {games.map((game, gameNum) => {
+                    let team1 = game.team1
+                      ? `${userMap.get(game.team1?.user1)} & ${userMap.get(
+                          game.team1?.user2
+                        )}`
+                      : "BYE";
+                    let team2 = game.team2
+                      ? `${userMap.get(game.team2?.user1)} & ${userMap.get(
+                          game.team2?.user2
+                        )}`
+                      : "BYE";
+
+                    return (
+                      <tr key={gameNum}>
+                        <td>{team1}</td>
+                        <td>{team2}</td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </Table>
+            </React.Fragment>
+          ))}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Cancel
+          </Button>
+          <Button
+            data-testid="confirm-creation"
+            variant="primary"
+            onClick={createGames}
+          >
+            Confirm Round Robin
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </Container>
   );
 }
